8f8e7487fd5ce63a6125f80fce15a8c895c3cf9a
danig89 <74435846+danig89@users.noreply.github.com>
2021-03-05 18:30:02 -0500

    Update challenge_logic.js.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -129,13 +129,13 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 5) {
-      return "#e30022";
+      return "#ea2c2c";
     }
     if (magnitude > 4) {
-      return "#ed872d";
+      return "#ea822c";
     }
     if (magnitude < 4) {
-      return "#ff9966";
+      return "#ee9c00";
     }
   }
   
